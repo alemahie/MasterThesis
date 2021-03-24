9f8186725ce8e52e1995b97ef5fd7ad6f0340fc8
alemahie <antoine.lemahieu@ulb.be>
2021-03-24 09:41:24 +0100

    New articles to read

--- Code/main.py
+++ Code/main.py
@@ -7,5 +7,5 @@ import MindBigDataVisualizer
 
 if __name__ == "__main__":
     print("Mind Big Data Visualizer")
-    # MindBigDataVisualizer.plot_imagenet_file()
-    MindBigDataVisualizer.plot_mnist_epoc_file()
+    MindBigDataVisualizer.plot_imagenet_file()
+    # MindBigDataVisualizer.plot_mnist_epoc_file()
